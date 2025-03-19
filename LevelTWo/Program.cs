0509c4ced90d87695b260856f65d6ab465da3506
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-03-19 23:30:08 +0000

    first commit

---
+++ LevelTWo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Spectre.Console;
+
+namespace LevelTWo
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+           CanvasImage image = new CanvasImage("tux.jpg");
+           image.MaxWidth(24);
+           AnsiConsole.Write(image);
+        }
+    }
+}
