a916a195ed78f29c873dc46a5d7e861311534d0a
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-03-19 23:01:55 +0000

    Add method doc

--- LevelOne/MainIsHere.cs
+++ LevelOne/MainIsHere.cs
@@ -4,6 +4,9 @@ namespace LevelOne
 {
     public class Program
     {
+        /// <summary>
+        /// the main method, but different.
+        /// </summary>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
