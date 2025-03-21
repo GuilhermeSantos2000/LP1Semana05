61a9906d5f0bb25837aca41ce3c7d980a5a5d882
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-03-21 17:49:48 +0000

    create CarStats project (Retroactively)

---
+++ CarStats/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Spectre.Console;
+using System.Collections.Generic;
+using Bogus;
+
+namespace CarStats
+{
+    public class Program
+    {
+       private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
