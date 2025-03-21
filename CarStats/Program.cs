308399591e11c9d419371b622105c6fcd88ad14a
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-03-21 17:52:52 +0000

    Add the solved exercise

--- CarStats/Program.cs
+++ CarStats/Program.cs
@@ -9,7 +9,29 @@ namespace CarStats
     {
        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            int numberOfBrands = int.Parse(args[0]);
+            Randomizer.Seed = new Random(numberOfBrands);
+            
+            Faker faker = new Faker();
+            List<string> brands = new List<string>();
+            Random random = new Random();
+
+            for (int i = 0; i < numberOfBrands; i++)
+            {
+                brands.Add(faker.Vehicle.Manufacturer());
+            }
+
+            BarChart barChart = new BarChart()
+                .Width(60)
+                .Label("[blue bold underline]Car Sales[/]")
+                .CenterLabel();
+
+            foreach (string brand in brands)
+            {
+                barChart.AddItem(brand, random.Next(1, 20), Color.Teal);
+            }
+
+            AnsiConsole.Write(barChart);
         }
     }
 }
